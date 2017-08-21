7c0fef3f0cbddc5ed5681cff3137d080cd556d62
Administrator <Administrator@A-201704201602>
2017-08-21 21:35:21 +0800

    IsMoyu

--- src/com/gay/gay.java
+++ src/com/gay/gay.java
@@ -1,10 +1,11 @@
 package com.gay;
 
 public class gay {
-public static void main(String[] args) {
-	System.out.println("欢迎加入亚洲最大同性交友群组：群组成员如下：（昵称）");
-	System.out.println("QMSD");
-	System.out.println("欢迎来到qm的亚洲max gayTeams");
-	System.out.println("桑井空最大视频网站");
-}
+	public static void main(String[] args) {
+		System.out.println("欢迎加入亚洲最大同性交友群组：群组成员如下：（昵称）");
+		System.out.println("QMSD");
+		System.out.println("欢迎来到qm的亚洲max gayTeams");
+		System.out.println("桑井空最大视频网站");
+		System.out.println("IsMoyu");
+	}
 }
