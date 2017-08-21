038b35aaa02eb2dfd28d293efae8c79696c0e263
zdl <zdl@666.om>
2017-08-21 20:51:51 +0800

    gay

--- src/com/gay/gay.java
+++ src/com/gay/gay.java
@@ -4,5 +4,6 @@ public class gay {
 public static void main(String[] args) {
 	System.out.println("欢迎加入亚洲最大同性交友群组：群组成员如下：（昵称）");
 	System.out.println("QMSD");
+	System.out.println("欢迎来到qm的亚洲max gayTeams");
 }
 }
